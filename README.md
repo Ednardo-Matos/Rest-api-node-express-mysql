8b7464e5804ea1ae21049ac0cd18a71d90bb81e9
Ednardo-Matos <ednardoinacio@outlook.com>
2022-04-05 11:55:10 -0300

    adicionando arquivo infraestrutura

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Rest-api-node-express-mysql
 # npm install --save-dev nodemon
 # npm install consign
-# npm install body-parse
+# npm install mysql2
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const customExpress = require('./config/customExpress')// importando customExpress
-const port = 3000;// porta do servidor
-const app = customExpress();//atribuindo customExpress ao app
+const port = 3000;
+const conexao = require('./infraestrutura/conexao');//importando a conexão
+const tabelas = require('./infraestrutura/tabelas');
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta: ${port}`);
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro);
+
+    }else {
+        console.log("Banco conectado com sucesso");
+
+        tabelas.init(conexao)
+
+        const app = customExpress();//atribuindo customExpress ao app
+        
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta: ${port}`);
+        });
+        
+    }
 });
+
 
